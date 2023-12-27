7fc6c1c66f4f21e3f36ae63e1ea530995cea09e8
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-12-27 23:33:48 +0530

    chnadan bhai

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$username ='root';
+$password ='Admin1234#@';
+$dbname ='accountant_details';
+$hostname ='localhost';
+
+
+$config =mysqli_connect($hostname, $username, $password, $dbname);
+
+?>
